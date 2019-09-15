3fa63b929e1c989f8ee495eecfcc3d6648a0ecb3
Hanna Endrias <hannaendrias@dhcp-18-21-227-44.dyn.mit.edu>
2019-09-15 10:02:33 -0400

    attempt 2

---
+++ app/routes.py
@@ -0,0 +1,25 @@
+from app import app
+from flask import render_template
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+@app.route('/login/')
+@app.route('/hello/<name>')
+def login(name=None):
+    return render_template('login.html')
+
+@app.route('/signup/')
+def signup(name=None):
+    return render_template('signup.html')
+
+@app.route('/udata/')
+def userin(name=None):
+    return render_template('udata.html') 
+
+@app.route('/ulogin/')
+def udata(name=None):
+    return render_template('ulogin.html')
